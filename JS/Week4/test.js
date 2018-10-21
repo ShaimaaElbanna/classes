f98ecb7c2f122b0f380f7f0566d365802e52f3c4
ShaimaaElbanna <ShaimaaElbanna@users.noreply.github.com>
2018-10-21 13:17:02 +0200

    go

---
+++ JS/Week4/test.js
@@ -0,0 +1,32 @@
+console.log('hello')
+
+
+
+
+
+
+
+Log()
+
+let sum = Add(10, 15)
+console.log(sum)
+
+function Log() {
+
+    let global = 'earth'
+
+    let name = 'ben'
+    console.log(global)
+}
+
+function Add(a, b) {
+    let global = 'Saturn'
+    return a + b + global
+}
+
+debugger
+
+Log()
+
+console.log(global)
+
